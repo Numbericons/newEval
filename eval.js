b2f154bc6219aecb18a0460bd85f3b13a2ab5fe2
Numbericons <zachjoliver@gmail.com>
2021-02-16 23:16:59 -0800

    first commit

---
+++ eval.js
@@ -0,0 +1,15 @@
+//iterate strong string
+//if we find an opening parenthesis, find the substring that contains the closing bracket
+//  make a recursive call to the function with the above substring
+
+
+//track first term, operator and second term
+
+//if char is a number
+
+//if char is a mathematical operator
+//  
+
+var calc = function (expression) {
+  // evaluate `expression` and return result
+};
